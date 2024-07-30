f2c04fdd237052bc312495a6e3878ae29b362af6
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2024-07-30 12:05:59 +0200

    localstorage services

---
+++ src/app/services/tasks.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TasksService {
+
+  private localSotorageKey = 'todoList';
+
+  getTasks() {
+    return JSON.parse(localStorage.getItem(this.localSotorageKey) as string) || [];
+  }
+
+  addTask(task: string){
+    const tasks = this.getTasks();
+    tasks.push(task);
+    localStorage.setItem(this.localSotorageKey, JSON.stringify(tasks));
+  }
+
+  deleteTask(index: number){
+    const tasks = this.getTasks();
+    tasks.splice(index, 1);
+    localStorage.setItem(this.localSotorageKey, JSON.stringify(tasks));
+  }
+
+}
